5a8527b8f24b5e2cfe718d428716a167fa6303c6
Ajay Kumar <ajax@Ajays-MacBook-Air.local>
2023-05-23 20:46:34 +0530

    feat: docker file and docker compose file added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          bye there how are you dear!!!
         </p>
         <a
           className="App-link"
